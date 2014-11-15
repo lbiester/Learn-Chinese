cbce68afe90d24ef164b6221f41e352fc798f861
lbiester <laurajpb@gmail.com>
2014-11-15 16:06:18 +0800

    Updates to start database, added jQuery

--- app/models.py
+++ app/models.py
@@ -1,1 +1,20 @@
 # models.py
+
+from app import db
+
+class Word(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    traditional = db.Column(db.String(256))
+    simplified = db.Column(db.String(256))
+    pinyin = db.Column(db.String(256))
+    english = db.Column(db.String(200))
+
+class Set(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(40))
+    user_id = db.Column(db.Integer, foreign_key=True)
+
+
+class WordSet(db.Model):
+    word_id = db.Column(db.Integer, ForeignKey('Word.id'), primary_key=True)
+    set_id = db.Column(db.Integer, ForeignKey('Set.id'), primary_key=True)
