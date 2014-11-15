d632157487b23eb93ba760d6817a38d9364f2ab9
Valerie Lambert <lambertv@carleton.edu>
2014-11-15 16:31:52 +0800

    added db script

--- app/views.py
+++ app/views.py
@@ -6,4 +6,4 @@ from flask import render_template
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', title='Home') 
+    return render_template('index.html', title='Home')
