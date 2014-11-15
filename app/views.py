94a398bf9b5126eb6ccba2734cbdb7a1303d3f72
lbiester <laurajpb@gmail.com>
2014-11-16 02:11:48 +0800

    Added all data. Created set pages

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 # This Python file uses the following encoding: utf-8
 
-from app import app
+from app import app, models, db
 from models import *
 from flask import render_template, request, redirect, jsonify
 from flask_user import login_required
@@ -14,8 +14,17 @@ def index():
 @app.route('/addset', methods=['GET', 'POST'])
 def addset():
     if request.method == 'POST':
+        user = models.User.query.first()
         list = request.form.getlist('input-word')
-        print list
+        set = models.Set(request.form['set-name'], user)
+        for input in list:
+            if input != "":
+                word = models.Word.query.filter_by(simplified=input).first()
+                set.words.append(word)
+        for word in set.words:
+            print word
+        db.session.add(set)
+        db.session.commit()
         return redirect('/')
     return render_template('addset.html')
 
@@ -26,8 +35,13 @@ def getWord(word):
     return_data = {'traditional': query_result.traditional, 'pinyin': query_result.pinyin, 'english': query_result.english}
     return jsonify(return_data)
 
+@app.route('/set/<id>')
+def set(id):
+    set = Set.query.filter_by(id=id).first()
+    return render_template('set.html', name=set.name, set=set.words)
+
+
 @app.route('/cards/<id>')
 def cards(id):
-    fake_set = [{'traditional': '你好', 'simplified': 'nihao-S', 'pinyin': 'ni3 hao3', 'english': 'hello'}]
-    fake_set.append({'traditional': 'wo-T', 'simplified': 'wo-S', 'pinyin': 'wo3', 'english': 'I'})
-    return render_template('cards.html', set=fake_set)
+    set = Set.query.filter_by(id=id).first()
+    return render_template('cards.html', name=set.name, set=set.words)
--- createDict.py
+++ createDict.py
@@ -9,21 +9,26 @@ import codecs
 import re
 f = open('chinese-dictionary.txt','r')
 i =0
+create_engine('sqlite:///app.db?encoding=Unicode')
 for line in f.readlines():
-    if i < 1200:
+    try:
         completecompetition = line.split(" ",2)
         traditional = completecompetition[0]
         simplified = completecompetition[1]
         pinyin = re.findall(r'\[([^]]*)\]', completecompetition[2])
         english = re.findall(r'\/([^]]*)\/', completecompetition[2])
-        create_engine('sqlite:///app.db?encoding=Unicode')
+
 
         i+=1
+        if i % 1000 == 0:
+            print float(i / 110255.0)
         if not len(english) == 0:
-            
+
             new_word = models.Word(traditional=unicode(traditional, "utf-8"),simplified=unicode(simplified,'utf-8'),pinyin=unicode(pinyin[0],'utf-8'),english=unicode(english[0],'utf-8'))
             db.session.add(new_word)
             db.session.commit()
+    except:
+        pass
 
 #To query everything in the Word table
 #word=models.Word.query.all()
