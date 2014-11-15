a589aba8c35a4310d8c0c52f91e0c6a4fe0b8c96
Valerie Lambert <lambertv@carleton.edu>
2014-11-15 22:40:36 +0800

    fixed models again

--- app/models.py
+++ app/models.py
@@ -27,7 +27,7 @@ class User(db.Model, UserMixin):
         return self.is_enabled
     
     def __repr__(self):
-        return '<User %r>' % (self.nickname)
+        return '<User %r>' % (self.username)
 
 wordset = db.Table('wordset',
     db.Column('word_id', db.Integer, db.ForeignKey('word.id')),
@@ -56,6 +56,9 @@ class Set(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(40))
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Set %r, %r>' % (self.name, self.user_id)
 
     def __init__(self,name,user):
 	self.name = name
